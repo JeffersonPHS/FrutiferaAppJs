ea457b7904a7b0cc45045c5710cd547170389fc2
Jefferson <fvesp7458@gmail.com>
2025-08-18 09:56:32 -0300

    Projeto Bootstrap

--- src/cardapio.js
+++ src/cardapio.js
@@ -1,4 +1,38 @@
-const form = document.getElementById('button-modal');
+const buttonModal = document.getElementById('button-modal');
+const form = document.getElementById('container-form');
+const tapela = document.querySelector("#table-container tbody");
+
+
+buttonModal.addEventListener('click', () => {
+  const formData = new FormData(form);
+  const formValues = Object.fromEntries(formData);
+  localStorage.setItem("formValues", JSON.stringify(formValues));
+  console.log("Dados", formValues)
+  
+  console.log(localStorage.getItem("formValues"));
+
+  
+  tapela.innerHTML = `
+        <td>${formValues.quantidade}</td>
+        <td>${formValues.cafe}</td>
+        <td>${formValues.quantidade}</td>
+        <td>${formValues.preco}</td>
+  `;
+  
+  form.reset();
+});
+
+
+
+
+
+
+
+
+
+
+
+/* const form = document.getElementById('button-modal');
 
   form.addEventListener('click', function (event) {
     event.preventDefault(); // Impede o comportamento padrão de recarregar a página
@@ -6,14 +40,17 @@ const form = document.getElementById('button-modal');
     const email = document.getElementById('email').value;
     console.log(nome); // Exibe os dados como objeto
      console.log(email);
-  });
+  }); */
 
 
 
 
+  /* const form = document.getElementById('button-modal');
 
-
-
-
-
-
+  form.onclick =  function () {
+   // Impede o comportamento padrão de recarregar a página
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    console.log(nome); // Exibe os dados como objeto
+     console.log(email);
+  }; */
